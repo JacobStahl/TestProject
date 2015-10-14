b6b351305f96e2791f8d8ba48b9c8a80447dcf72
Albin <afagernes@bredband.net>
2015-10-14 12:29:37 +0200

    Added some tests for kund and made sure all tests finish successfully.

--- INTE-KvittoProjekt/src/Kund.java
+++ INTE-KvittoProjekt/src/Kund.java
@@ -16,6 +16,44 @@ public class Kund {
 		
 	}
 	
+	public int getKundNr(){
+		return kundNr;
+	}
 	
+	public void setKundNr(int kundNr){
+		this.kundNr = kundNr;
+	}
+	
+	public String getForNamn(){
+		return forNamn;
+	}
+	
+	public void setForNamn(String forNamn){
+		this.forNamn = forNamn;
+	}
+	
+	public String getEfterNamn(){
+		return efterNamn;
+	}
+	
+	public void setEfterNamn(String efterNamn){
+		this.efterNamn = efterNamn;
+	}
+	
+	public String getTelefonNummer(){
+		return telefonNummer;
+	}
+	
+	public void setTelefonNummer(String telefonNummer){
+		this.telefonNummer = telefonNummer;
+	}
+	
+	public String getAdress(){
+		return adress;
+	}
+	
+	public void setAdress(String adress){
+		this.adress = adress;
+	}
 	
 }
--- INTE-KvittoProjekt/testCases/kundTest.java
+++ INTE-KvittoProjekt/testCases/kundTest.java
@@ -1,32 +1,57 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class kundTest {
-
+	
+	private Kund k;
+	
+	int kundNr;
+	String forNamn;
+	String efterNamn;
+	String telefonNummer;
+	String adress;
+	
+	@Before
+	public void setUp(){
+		kundNr = 123;
+		forNamn = "Johan";
+		efterNamn = "Svensson";
+		telefonNummer = "070 253 25 34";
+		adress = "Stenvägen 4";
+		
+		k = new Kund(kundNr, forNamn, efterNamn, telefonNummer, adress);
+	}
+	
 	@Test
 	public void checkIfNull() {
-		int kundNr = 123;
-		String forNamn = "Johan";
-		String efterNamn = "Svensson";
-		String telefonNummer = "070 253 25 34";
-		String adress = "Stenvägen 4";
-		
-		Kund k = new Kund(kundNr, forNamn, efterNamn, telefonNummer, adress);
-		
 		assertNotNull(k);
 	}
 	
 	@Test
 	public void checkConstructorInput(){
-		int kundNr = 123;
-		String forNamn = "Johan";
-		String efterNamn = "Svensson";
-		String telefonNummer = "070 253 25 34";
-		String adress = "Stenvägen 4";
+		assertEquals(kundNr, k.getKundNr());
+		assertEquals(forNamn, k.getForNamn());
+		assertEquals(efterNamn, k.getEfterNamn());
+		assertEquals(telefonNummer, k.getTelefonNummer());
+		assertEquals(adress, k.getAdress());
+	}
+	
+	@Test
+	public void checkSetMethods(){
+		kundNr = 234;
+		forNamn = "Stefan";
+		efterNamn = "Andersson";
+		telefonNummer = "070 503 27 13";
+		adress = "Trävägen 6";
 		
-		Kund k = new Kund(kundNr, forNamn, efterNamn, telefonNummer, adress);
+		k.setKundNr(kundNr);
+		k.setForNamn(forNamn);
+		k.setEfterNamn(efterNamn);
+		k.setTelefonNummer(telefonNummer);
+		k.setAdress(adress);
 		
 		assertEquals(kundNr, k.getKundNr());
 		assertEquals(forNamn, k.getForNamn());
