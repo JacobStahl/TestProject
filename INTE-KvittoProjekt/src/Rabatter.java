7c9b29ebf7bc6b3073ede01b1475027a0e0c8180
Gezus <pyro.fg@gmail.com>
2015-10-18 22:06:18 +0200

    RabattKlass och RabattTest

--- INTE-KvittoProjekt/src/Rabatter.java
+++ INTE-KvittoProjekt/src/Rabatter.java
@@ -1,4 +1,52 @@
 
 public class Rabatter {
 
+	private String rabattTyp;
+	private int rabattProcent;
+	private int rabattMultipel; 
+	
+	
+	public Rabatter(String rabattTyp, int rabattProcent, int rabattMultipel)
+	{
+		this.rabattTyp = rabattTyp;
+		this.rabattProcent = rabattProcent;
+		this.rabattMultipel = rabattMultipel;
+	}
+	
+	public void setrabattTyp(String rabattTyp)
+	{
+		this.rabattTyp = rabattTyp;		
+	}
+	
+	public String getrabattTyp()
+	{
+		return rabattTyp;
+	}
+	
+	public void setrabattProcent(int rabattProcent)
+	{
+		this.rabattProcent = rabattProcent;		
+	}
+	
+	public int getrabattProcent()
+	{
+		return rabattProcent;
+	}
+	
+	public void setrabattMultipel(int rabattMultipel)
+	{
+		this.rabattMultipel = rabattMultipel;		
+	}
+
+	
+	public int getrabattMultipel()
+	{
+		return rabattMultipel;
+	}
+	
+	
+	
+	
+	
+	
 }
