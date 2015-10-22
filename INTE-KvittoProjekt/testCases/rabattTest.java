04a18bf9eb374f8e534686e1f81c64839c7f1730
Laughlyn <laughlyn@hotmail.com>
2015-10-22 12:06:28 +0200

    Ekvivalensklasspartionstester (Härligt ord)

--- INTE-KvittoProjekt/testCases/rabattTest.java
+++ INTE-KvittoProjekt/testCases/rabattTest.java
@@ -9,6 +9,7 @@ public class rabattTest {
 	
 	private Kund k;
 	private Produkt p;
+	private Produkt q;
 	
 	private int kundNr;
 	private String forNamn;
@@ -52,7 +53,7 @@ public class rabattTest {
 		produktNamn = "Gurka";
 		produktTyp = "Grönsak";
 		pris = 5.5f;
-		mangd = 1;
+		mangd = 6;
 		rabattTypProdukt = 0;
 		rabattKronor = 0;
 		
@@ -61,7 +62,7 @@ public class rabattTest {
 		
 		double kalk = rabatt.beraknaProduktRabatt(p);
 		
-		assertEquals(5.5f, kalk, 0f);
+		assertEquals(11f, kalk, 0f);
 	}
 	
 	@Test
@@ -70,7 +71,7 @@ public class rabattTest {
 		produktNamn = "Gurka";
 		produktTyp = "Grönsak";
 		pris = 5.5f;
-		mangd = 3;
+		mangd = 1;
 		rabattTypProdukt = 1;
 		rabattKronor = 3.2f;
 		
@@ -80,6 +81,26 @@ public class rabattTest {
 		double kalk = rabatt.beraknaProduktRabatt(p);
 		
 		assertEquals((3.2f*mangd), kalk, 0f);
+	}
+	
+	@Test //Testar några ekvivalensklasspartitioner
+	public void testTreForTvaLoop(){
+		produktNr = 1;
+		produktNamn = "Gurka";
+		produktTyp = "Grönsak";
+		pris = 5.5f;
+		mangd = 0;
+		rabattTypProdukt = 0;
+		rabattKronor = 0;
+		
+		q = new Produkt(produktNr, produktNamn, produktTyp, pris, mangd, rabattTypProdukt, rabattKronor);
+		Rabatt rabatt = new Rabatt();
+		
+		for(int i = 0;i<13;i++){
+			double kalk = rabatt.beraknaProduktRabatt(q);
+			assertEquals((q.getMangd()/3)*5.5f, kalk, 0f);
+			q.setMangd(q.getMangd()+3);
+		}
 	}
 	
 }
